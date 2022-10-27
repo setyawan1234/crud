31e9fb5c164f89b891f4ea0822b43369f74c55fa
setyawan1234 <twentyzee1@gmail.com>
2022-10-27 10:05:47 +0700

    3 commit

--- create.php
+++ create.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Form Pendaftaran Peserta</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
+
 <body>
-<div class="container">
-    <?php
+    <div class="container">
+        <?php
     include "koneksi.php";
     function input($data) {
         $data = trim($data);
@@ -37,35 +40,36 @@
 
     }
     ?>
-    <h2>Input Data</h2>
+        <h2>Input Data</h2>
 
 
-    <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-        <div class="form-group">
-            <label>Nama:</label>
-            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
+        <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+            <div class="form-group">
+                <label>Nama:</label>
+                <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
 
-        </div>
-        <div class="form-group">
-            <label>Sekolah:</label>
-            <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required/>
-        </div>
-       <div class="form-group">
-            <label>Jurusan :</label>
-            <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required/>
-        </div>
-                </p>
-        <div class="form-group">
-            <label>No HP:</label>
-            <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required/>
-        </div>
-        <div class="form-group">
-            <label>Alamat:</label>
-            <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
-        </div>       
+            </div>
+            <div class="form-group">
+                <label>Sekolah:</label>
+                <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required />
+            </div>
+            <div class="form-group">
+                <label>Jurusan :</label>
+                <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required />
+            </div>
+            </p>
+            <div class="form-group">
+                <label>No HP:</label>
+                <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required />
+            </div>
+            <div class="form-group">
+                <label>Alamat:</label>
+                <textarea name="alamat" class="form-control" rows="5" placeholder="Masukan Alamat" required></textarea>
+            </div>
 
-        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </body>
+
 </html>
--- update.php
+++ update.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Form Pendaftaran Anggota</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
 </head>
+
 <body>
-<div class="container">
-    <?php
+    <div class="container">
+        <?php
 
     include "koneksi.php";
 
@@ -60,36 +63,37 @@
     }
 
     ?>
-    <h2>Update Data</h2>
+        <h2>Update Data</h2>
 
 
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-        <div class="form-group">
-            <label>Nama:</label>
-            <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+            <div class="form-group">
+                <label>Nama:</label>
+                <input type="text" name="nama" class="form-control" placeholder="Masukan Nama" required />
 
-        </div>
-        <div class="form-group">
-            <label>Sekolah:</label>
-            <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required/>
-        </div>
-        <div class="form-group">
-            <label>Jurusan :</label>
-            <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required/>
-        </div>
-        <div class="form-group">
-            <label>No HP:</label>
-            <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required/>
-        </div>
-        <div class="form-group">
-            <label>Alamat:</label>
-            <textarea name="alamat" class="form-control" rows="5"placeholder="Masukan Alamat" required></textarea>
-        </div>
+            </div>
+            <div class="form-group">
+                <label>Sekolah:</label>
+                <input type="text" name="sekolah" class="form-control" placeholder="Masukan Nama Sekolah" required />
+            </div>
+            <div class="form-group">
+                <label>Jurusan :</label>
+                <input type="text" name="jurusan" class="form-control" placeholder="Masukan Jurusan" required />
+            </div>
+            <div class="form-group">
+                <label>No HP:</label>
+                <input type="text" name="no_hp" class="form-control" placeholder="Masukan No HP" required />
+            </div>
+            <div class="form-group">
+                <label>Alamat:</label>
+                <textarea name="alamat" class="form-control" rows="5" placeholder="Masukan Alamat" required></textarea>
+            </div>
 
-        <input type="hidden" name="id_peserta" value="<?php echo $data['id_peserta']; ?>" />
+            <input type="hidden" name="id_peserta" value="<?php echo $data['id_peserta']; ?>" />
 
-        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 </body>
+
 </html>
